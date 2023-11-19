076046a30166a3471c34a069378f425a56d2b187
Papp Levente <papp.levente2003@gmail.com>
2023-11-19 19:07:08 +0100

    Forgot to add this class, whoops

---
+++ src/main/java/papplevaa/UndoableTextArea.java
@@ -0,0 +1,29 @@
+package papplevaa;
+
+import javax.swing.*;
+import javax.swing.undo.UndoManager;
+
+public class UndoableTextArea extends JTextArea {
+    private UndoManager undoManager;
+    public UndoableTextArea(String content) {
+        super(content);
+        this.undoManager = new UndoManager();
+        this.undoManager.setLimit(100);
+    }
+
+    public UndoManager getUndoManager() {
+        return this.undoManager;
+    }
+
+    public void undo() {
+        if(this.undoManager.canUndo()) {
+            this.undoManager.undo();
+        }
+    }
+
+    public void redo() {
+        if(this.undoManager.canRedo()) {
+            this.undoManager.redo();
+        }
+    }
+}
