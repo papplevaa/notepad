3b0646cef8601dc1749f29041c70fcb6787683fe
Papp Levente <papp.levente2003@gmail.com>
2023-11-26 19:31:19 +0100

    Add comments to View

--- src/main/java/papplevaa/notepad/ui/View.java
+++ src/main/java/papplevaa/notepad/ui/View.java
@@ -13,21 +13,41 @@ import java.awt.event.*;
 import java.io.File;
 import java.util.function.IntConsumer;
 
+/**
+ * The graphical user interface class for a notepad application.
+ * Manages the main frame, tabbed panes, menus, and their interactions.
+ */
 public class View {
+    /** The callback handler for communication with the controller. */
     private CallbackHandler callback;
+    /** The main frame of the Notepad application. */
     private JFrame frame;
+    /** The tabbed pane containing individual tabs for text file. */
     private JTabbedPane tabbedPane;
 
-    /* Methods for initialization */
+    /**
+     * Constructs a new View, initializing the main frame and tabbed pane.
+     */
     public View() {
         this.frame = new JFrame("Notepad");
         this.tabbedPane = new JTabbedPane();
     }
 
+    /**
+     * Registers the callback handler for communication with the controller.
+     *
+     * @param callback The callback handler to register.
+     */
     public void registerCallback(CallbackHandler callback) {
         this.callback = callback;
     }
 
+    /**
+     * Initializes the view with the provided model, setting up the main frame,
+     * menus, and the tabbed pane with tabs from the model.
+     *
+     * @param model The model containing data to initialize the view.
+     */
     public void initialize(Model model) {
         this.initFrame(model);
         this.initMenu();
@@ -35,6 +55,9 @@ public class View {
         this.setDarkMode(model.isDarkMode());
     }
 
+    /**
+     * Runs the view on the event dispatch thread, making the frame visible and focusing on the selected text area.
+     */
     public void run() {
         SwingUtilities.invokeLater(() -> {
             this.frame.setVisible(true);
@@ -45,15 +68,27 @@ public class View {
         });
     }
 
-    /* Method to reach the selected text area */
+    /**
+     * Retrieves the selected text area from the currently active tab.
+     *
+     * @return The selected text area or null if no tab is selected.
+     */
     public UndoableTextArea getSelectedTextArea() {
-        JScrollPane scrollPane = (JScrollPane) tabbedPane.getSelectedComponent();
-        if(scrollPane == null)
-            return null;
-        return (UndoableTextArea) scrollPane.getViewport().getView();
+        UndoableTextArea result = null;
+        JScrollPane scrollPane = (JScrollPane) this.tabbedPane.getSelectedComponent();
+        if (scrollPane != null) {
+            result = (UndoableTextArea) scrollPane.getViewport().getView();
+        }
+        return result;
     }
 
-    /* Methods for updates */
+    /**
+     * Updates the title of the tab at the specified index, adding a star if unsaved.
+     *
+     * @param index   The index of the tab.
+     * @param title   The new title for the tab.
+     * @param starred True if the tab is starred, false otherwise.
+     */
     public void updateTitleAt(int index, String title, boolean starred) {
         if(starred) {
             this.tabbedPane.setTitleAt(index, "*" + title);
@@ -62,11 +97,22 @@ public class View {
         }
     }
 
+    /**
+     * Changes the selected tab to the one at the specified index and focuses on its text area.
+     *
+     * @param activeTab The index of the tab to be selected.
+     */
     public void changeSelectedTab(int activeTab) {
         this.tabbedPane.setSelectedIndex(activeTab);
         this.getSelectedTextArea().requestFocus();
     }
 
+    /**
+     * Adds a new tab with the given name and content to the tabbed pane.
+     *
+     * @param name    The name of the new tab.
+     * @param content The initial current content of the new tab.
+     */
     public void addTab(String name, String content) {
         UndoableTextArea textArea = new UndoableTextArea(content);
         this.setupCustomizedTextArea(textArea);
@@ -74,10 +120,20 @@ public class View {
         this.tabbedPane.add(name, scrollPane);
     }
 
+    /**
+     * Removes the tab at the specified index from the tabbed pane.
+     *
+     * @param tabIndex The index of the tab to be removed.
+     */
     public void removeTab(int tabIndex) {
         this.tabbedPane.remove(tabIndex);
     }
 
+    /**
+     * Sets the dark mode for the entire application, updating the look and feel accordingly.
+     *
+     * @param darkMode True to enable dark mode, false to disable.
+     */
     public void setDarkMode(boolean darkMode) {
         try {
             if(darkMode) {
@@ -91,11 +147,19 @@ public class View {
         SwingUtilities.updateComponentTreeUI(this.frame);
     }
 
+    /**
+     * Closes the main frame of the application.
+     */
     public void closeFrame() {
         this.frame.dispose();
     }
 
-    /* Methods for showing dialogs */
+    /**
+     * Opens a file chooser dialog for choosing a file based on the specified dialog type.
+     *
+     * @param dialogType The type of the file chooser dialog (SAVE or OPEN).
+     * @return The selected file or null if the dialog is canceled.
+     */
     public File chooseFile(ChooseFileDialogType dialogType) {
         File selectedFile = null;
         JFileChooser fileChooser = new JFileChooser();
@@ -110,6 +174,11 @@ public class View {
         return selectedFile;
     }
 
+    /**
+     * Displays a confirmation dialog asking the user if they want to save changes.
+     *
+     * @return The user's choice from the confirmation dialog.
+     */
     public ConfirmDialogOptions showConfirmDialog() {
         int result = JOptionPane.showConfirmDialog(this.frame, "Do you want to save changes?", "Notepad", JOptionPane.YES_NO_CANCEL_OPTION);
         return ConfirmDialogOptions.getByValue(result);
@@ -118,6 +187,12 @@ public class View {
     /* ---------------------- *
      * INITIALIZATION METHODS *
      * ---------------------- */
+
+    /**
+     * Initializes the main frame with custom close operation, size, and adds a component listener for resizing.
+     *
+     * @param model The model containing data to set the initial frame size.
+     */
     private void initFrame(Model model) {
         // Set custom close operation here
         this.frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
@@ -140,6 +215,9 @@ public class View {
         });
     }
 
+    /**
+     * Initializes the menu bar, creating the File and Edit menus, adding menu items and a button for changing the theme.
+     */
     public void initMenu() {
         /* ------ Menubar ------ */
         JMenuBar menuBar = new JMenuBar();
@@ -242,6 +320,11 @@ public class View {
         menuBar.add(button);
     }
 
+    /**
+     * Sets up the document listener and key bindings for a customized text area.
+     *
+     * @param textArea The text area to set up.
+     */
     private void setupCustomizedTextArea(UndoableTextArea textArea) {
         textArea.getDocument().addDocumentListener(new DocumentListener() {
             @Override
@@ -291,6 +374,11 @@ public class View {
         );
     }
 
+    /**
+     * Initializes the tabbed pane with custom properties and creates tabs based on the model.
+     *
+     * @param model The model containing data to create tabs.
+     */
     private void initTabbedPane(Model model) {
         this.tabbedPane.putClientProperty("JTabbedPane.tabClosable", true);
         this.tabbedPane.putClientProperty( "JTabbedPane.tabCloseCallback", (IntConsumer) tabIndex -> callback.closeTab());
@@ -301,6 +389,11 @@ public class View {
         this.frame.add(tabbedPane);
     }
 
+    /**
+     * Creates tabs in the tabbed pane based on the tabs in the provided model.
+     *
+     * @param model The model containing data to create tabs.
+     */
     private void createTabsFromModel(Model model) {
         int numberOfTabs = model.getNumberOfTabs();
         for(int index = 0; index < numberOfTabs; index++) {
