84c8f6b7a5fde9f373d11301e467d0674ce25fce
Papp Levente <papp.levente2003@gmail.com>
2023-11-26 20:00:26 +0100

    Add comments to the Main class and main method

--- src/main/java/papplevaa/notepad/Main.java
+++ src/main/java/papplevaa/notepad/Main.java
@@ -4,7 +4,18 @@ import papplevaa.notepad.controller.Controller;
 import papplevaa.notepad.model.Model;
 import papplevaa.notepad.ui.View;
 
+/**
+ * The entry point for the Notepad application. It initializes the Model, View, and Controller components,
+ * loads the saved model data, and starts the application.
+ */
 public class Main {
+    /**
+     * The main method that serves as the entry point for the Notepad application.
+     * Initializes the Model, View, and Controller components, loads the saved model data,
+     * and starts the application.
+     *
+     * @param args The command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         View view = new View();
         Model model = new Model();
