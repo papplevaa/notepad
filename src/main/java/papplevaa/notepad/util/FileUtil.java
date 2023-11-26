26b1b4d83de1f42ab4d37fb8f117742cf1fdf7b0
Papp Levente <papp.levente2003@gmail.com>
2023-11-26 20:03:19 +0100

    Add comments to FileUtil

--- src/main/java/papplevaa/notepad/util/FileUtil.java
+++ src/main/java/papplevaa/notepad/util/FileUtil.java
@@ -2,9 +2,20 @@ package papplevaa.notepad.util;
 
 import java.io.*;
 
+/**
+ * Utility class providing file-related operations for the Notepad application.
+ */
 public final class FileUtil {
-    private FileUtil() {}
+    private FileUtil() {
+        // Private constructor to prevent instantiation; utility class with static methods.
+    }
 
+    /**
+     * Reads the content of a file and returns it as a string.
+     *
+     * @param filePath The path to the file to be read.
+     * @return The content of the file as a string, or {@code null} if an error occurs during reading.
+     */
     public static String loadContent(File filePath) {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -20,6 +31,12 @@ public final class FileUtil {
         return content.toString();
     }
 
+    /**
+     * Writes the provided content to a file.
+     *
+     * @param content  The content to be written to the file.
+     * @param filePath The path to the file where the content will be saved.
+     */
     public static void saveContent(String content, File filePath) {
         try (FileWriter writer = new FileWriter(filePath)) {
             writer.write(content);
@@ -28,6 +45,13 @@ public final class FileUtil {
         }
     }
 
+    /**
+     * Serializes an object and saves it to a file.
+     *
+     * @param filePath The path to the file where the serialized object will be saved.
+     * @param object   The object to be serialized and saved.
+     * @param <T>      The type of the object.
+     */
     public static <T extends Serializable> void serialize(File filePath, T object) {
         try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(filePath))) {
             stream.writeObject(object);
@@ -36,11 +60,19 @@ public final class FileUtil {
         }
     }
 
-    public static <T extends Serializable> T deserialize(File filePath, Class<T> tclass) {
+    /**
+     * Deserializes an object from a file.
+     *
+     * @param filePath The path to the file containing the serialized object.
+     * @param tClass   The class of the object to be deserialized.
+     * @param <T>      The type of the object.
+     * @return The deserialized object, or {@code null} if an error occurs during deserialization.
+     */
+    public static <T extends Serializable> T deserialize(File filePath, Class<T> tClass) {
         try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream(filePath))) {
             Object object = stream.readObject();
-            if(tclass.isInstance(object)) {
-                return tclass.cast(object);
+            if(tClass.isInstance(object)) {
+                return tClass.cast(object);
             }
         } catch (IOException | ClassNotFoundException exception) {
             System.out.println("Failed to load data from previous session");
