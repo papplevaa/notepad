e110029aa70c8ec8b849a9fe964d41e74aecb928
Papp Levente <papp.levente2003@gmail.com>
2023-11-26 20:08:50 +0100

    Add comments to ChooseFileDialogType

--- src/main/java/papplevaa/notepad/util/ChooseFileDialogType.java
+++ src/main/java/papplevaa/notepad/util/ChooseFileDialogType.java
@@ -1,6 +1,16 @@
 package papplevaa.notepad.util;
 
+/**
+ * Enumeration representing the types of file chooser dialogs for choosing files.
+ * This enum includes options for saving a file and opening a file.
+ */
 public enum ChooseFileDialogType {
+    /**
+     * Option indicating that the file dialog is for saving a file.
+     */
     SAVE,
+    /**
+     * Option indicating that the file dialog is for opening a file.
+     */
     OPEN
 }
